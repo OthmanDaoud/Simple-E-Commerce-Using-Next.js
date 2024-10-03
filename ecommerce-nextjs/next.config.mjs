254709136c48ad093b2dd740c8cd171f294b5cbf
unknown <o.daoued@gmail.com>
2024-10-03 09:18:17 +0300

    final

--- ecommerce-nextjs/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["t3.ftcdn.net"], // Add the external image domain here
-  },
-};
-
-export default nextConfig;
